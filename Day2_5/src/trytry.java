b56abf3752c9b88b1a5799281b6e18dcfeaad2b5
monster <1908097012@qq.com>
2022-05-15 20:52:00 +0800

    try

---
+++ Day2_5/src/trytry.java
@@ -0,0 +1,12 @@
+public class trytry {
+    public static void main(String[] arg)
+    {
+        String[] Arr = {"How","Are","You?","Im","Fine"};
+        String a = new String("hahaha");
+        for(String test : Arr)
+        {
+            System.out.println(test);
+        }
+
+    }
+}
