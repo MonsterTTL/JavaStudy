0d567f4a93f818ba62549c62ddcfdfaed64695d4
monster <1908097012@qq.com>
2022-06-01 22:06:22 +0800

    增加了断言

--- Day15_异常/src/MainFunction.java
+++ Day15_异常/src/MainFunction.java
@@ -9,6 +9,7 @@ public class MainFunction {
 		{
 			System.out.print("Enter n: ");
 			int n = in.nextInt();
+			assert (n <= 8) : "n > 8!";
 			StackTraceTest.factorial(n);
 		}
 		catch(InputMismatchException e)
@@ -20,10 +21,18 @@ public class MainFunction {
 			System.out.println("Sorry!");
 			throw new InputMismatchException("输入信息类型不匹配");//再次抛出异常，并附加信息
 		 }
+		 catch(AssertionError c)
+		 {
+			 throw new AssertionError(c);
+		 }
 	    }catch(InputMismatchException d)
 		 {
 	    	System.out.println(d.getMessage());//打印出附加的信息
 			
+		 }
+	    catch(AssertionError e)
+		 {
+	    	System.out.println(e.getMessage());
 		 }
 	}
 
