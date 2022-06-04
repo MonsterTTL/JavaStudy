8de816e8d19e8ccdfcda8623e0ef153fa99046e6
monster <1908097012@qq.com>
2022-06-04 16:15:59 +0800

    通配符和泛型练习Day18

--- Day17_泛型++/src/PairTest3.java
+++ Day17_泛型++/src/PairTest3.java
@@ -1,3 +1,4 @@
+import inheritance.Employee;
 import inheritance.Manager;
 
 public class PairTest3 {
@@ -6,6 +7,45 @@ public class PairTest3 {
 		var ceo = new Manager("Gus Geedy",800000,2003,12,15);
 		var cfo = new Manager("Sid Snakey",600000,2003,12,15);
 		var builder = new Pair<Manager>(ceo,cfo);
+		printBuilddies(builder);
+		
+		ceo.setBonus(1000000);
+		cfo.setBonus(500000);
+		Manager[] managers = {ceo,cfo};
+		
+		var result = new Pair<Employee>();
+		minmaxBouns(managers,result);
+		System.out.println("first : "+result.getFirst().getName()
+				+ " second: "+result.getSecond().getName());
+		maxminBouns(managers,result);
+		System.out.println("first : "+result.getFirst().getName()
+				+ " second: "+result.getSecond().getName());
 	}
-
+	public static void printBuilddies(Pair<? extends Employee>p)
+	{
+		Employee first = p.getFirst();
+		Employee second = p.getSecond();
+		
+	}
+	public static void minmaxBouns(Manager[] a,Pair<? super Manager> result)
+	{
+		if(a.length == 0)return;
+		Manager min = a[0];
+		Manager max = a[0];
+		for(int i = 0;i < a.length;i++)
+		{
+			if(min.getBouns() > a[i].getBouns())
+				min = a[i];
+			if(max.getBouns() <= a[i].getBouns())
+				max = a[i];
+		}
+		result.setFirst(min);
+		result.setSecond(max);
+	}
+	
+	public static void maxminBouns(Manager[] a,Pair<? super Manager>result)
+	{
+		minmaxBouns(a,result);
+		PairAlg.swap(result);
+	}
 }
--- Day6_inherit2/src/inheritance/Manager.java
+++ Day6_inherit2/src/inheritance/Manager.java
@@ -22,5 +22,10 @@ public class Manager extends Employee {
 	{
 		bonus = b;
 	}
+	
+	public double getBouns()
+	{
+		return bonus;
+	}
 
 }
